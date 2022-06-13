b5fd8d3fe0d0b8be966d713de4ed912ab237c89f
Andre Carrano <andre@basequatro.com>
2022-06-13 08:34:16 -0300

    add animation

--- src/components/Hole.tsx
+++ src/components/Hole.tsx
@@ -1,9 +1,56 @@
+import { AnimatePresence, motion, useAnimation } from "framer-motion";
+import { useEffect } from "react";
+import { IMole } from "../types/definitions";
 
-const Hole = ({ isUp, isBomb }: any) => (
-    <div className="hole" data-testid="hole">
-        {isUp && !isBomb && <div className="mole" />}
-        {isUp && isBomb && <div className="molebomb" />}
-    </div>
-);
+const variants = {
+    start: {
+        y: 0,
+        transition: {
+            duration: 0.1,
+            ease: "easeInOut",
+        },
+    },
+
+    reset: {
+        y: 300,
+        transition: {
+            duration: 0.1,
+            ease: "easeInOut",
+        },
+    },
+};
+
+
+const Hole = ({ isUp, isBomb, onClick }: IMole) => {
+    const controls = useAnimation();
+
+    const handleClick = () => {
+        controls.start("reset");
+        onClick();
+    };
+
+    useEffect(() => {
+        if (isUp > 0) {
+            controls.start("start");
+        } else {
+            controls.start("reset");
+        }
+    }, [controls, isUp]);
+
+    return (
+        <div className="hole" data-testid="hole">
+            <AnimatePresence>
+                <motion.div
+                    data-testid={isBomb ? "moleup-bomb" : isUp ? "moleup" : "mole"}
+                    initial={{ y: 100 }}
+                    variants={variants}
+                    animate={controls}
+                    className={isBomb ? "mole isBomb" : "mole"}
+                    onClick={handleClick}
+                />
+            </AnimatePresence>
+        </div>
+    );
+};
 
 export default Hole;
